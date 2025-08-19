8e5ae302656c09ca4e1e11208e669a3416b73569
Shaik Jalil <jalilbtech@gmail.com>
2025-08-19 10:02:22 +0530

    python files

---
+++ example.py
@@ -0,0 +1,10 @@
+#------------------------------------------------------------------------#
+
+eggs = int(input("Enter no.of eggs: "))
+
+print(f"total no of dozens {eggs // 12},\nremaining eggs🥚 are {eggs % 12}")
+
+print(f"total eggs are {eggs}")
+
+#-------------------------------------------------------------------------#
+
